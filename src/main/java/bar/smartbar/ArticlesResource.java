b90a996a3d35c176970657418a72cc96c7c26c7c
giorgos-grekis <georgrekis.github@gmail.com>
2025-04-07 19:25:33 +0300

    add ArticlesResource and TableResource

---
+++ src/main/java/bar/smartbar/ArticlesResource.java
@@ -0,0 +1,38 @@
+package bar.smartbar;
+
+import bar.smartbar.api.ArticlesApi;
+import bar.smartbar.api.model.Article;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.List;
+
+public class ArticlesResource implements ArticlesApi {
+
+    private final Article article = new Article().name("TestArticle");
+
+    @Override
+    public Response articlesArticleIdDelete(String articleId) {
+        return Response.ok().build();
+    }
+
+    @Override
+    public Response articlesArticleIdGet(String articleId) {
+        return Response.ok(article).build();
+    }
+
+    @Override
+    public Response articlesArticleIdPut(String articleId, Article article) {
+        return Response.ok().build();
+    }
+
+    @Override
+    public Response articlesGet() {
+        return Response.ok(List.of(article)).build();
+    }
+
+    @Override
+    public Response articlesPost(Article article) {
+        return Response.created(URI.create("todo")).build();
+    }
+}
---
+++ src/main/java/bar/smartbar/TableResource.java
@@ -0,0 +1,39 @@
+package bar.smartbar;
+
+import bar.smartbar.api.TablesApi;
+import bar.smartbar.api.model.Table;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.List;
+
+
+public class TableResource implements TablesApi {
+
+    private final Table table = new Table().name("tableName");
+
+    @Override
+    public Response tablesGet() {
+        return Response.ok(List.of(table)).build();
+    }
+
+    @Override
+    public Response tablesPost(Table table) {
+        return Response.created(URI.create("todo")).build();
+    }
+
+    @Override
+    public Response tablesTableIdDelete(String tableId) {
+        return Response.ok().build();
+    }
+
+    @Override
+    public Response tablesTableIdGet(String tableId) {
+        return Response.ok(table).build();
+    }
+
+    @Override
+    public Response tablesTableIdPut(String tableId, Table table) {
+        return Response.ok().build();
+    }
+}
